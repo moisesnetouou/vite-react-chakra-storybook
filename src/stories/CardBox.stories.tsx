e8784552d827e9778a6042b3870cb9ae95836dd3
Moisés <moisesnetored@gmail.com>
2023-03-07 09:36:07 -0400

    feat: card box created

---
+++ src/stories/CardBox.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { CardBox } from "../components/CardBox";
+
+export default {
+  title: "Component/CardBox",
+  component: CardBox,
+} as Meta
+
+export const Default: StoryObj = {}
